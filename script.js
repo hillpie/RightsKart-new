21a56655e78badb5dee5133538c783de3ed48639
Rahil <94744804+hillpie@users.noreply.github.com>
2023-10-03 11:07:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,29 +3,6 @@
 //     console.log(dets.clientX, dets.clientY);
 // })
 
-// Throttling Function
-const btn=document.querySelector("#throttle");
-const throttleFunction=(func, delay)=>{
-  let prev = 0;
-  return (...args) => {
-    let now = new Date().getTime();
-    if(now - prev> delay){
-      prev = now;
-      return func(...args); 
-    }
-  }
-}
-document.querySelector("#center")
-.addEventListener("mousemove", throttleFunction((dets)=>{
-    var div = document.createElement("div");
-    div.classList.add('imagediv');
-    div.style.left = dets.clientX;
-    div.style.top = dets.clientY;
-    document.body.appendChild(div);
-}, 1500));
-
-
-
 
 
 var crsr = document.querySelector("#cursor")
